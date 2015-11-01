402c56387deb930215a0dce92b13249b2dd28186
Phil Chodrow <pchodrow@mit.edu>
2015-11-01 17:12:45 -0500

    added taz nodes to multiplex constructor

--- make_multiplex.py
+++ make_multiplex.py
@@ -1,5 +1,6 @@
 import multiplex as mx
-import utility as utility
+import utility
+import networkx as nx
 
 def main():
 	metro_dir = '1. data/metro'
@@ -8,17 +9,31 @@ def main():
 	street_dir = '1. data/street'
 	street_prefix = 'street'
 
+	taz_dir = '1. data/taz'
+	taz_file = '/taz_nodes.txt'
+
+	print 'Reading TAZ connector nodes'
+	taz = utility.graph_from_txt(nodes_file_name = taz_dir + taz_file,
+	                             nid = 'id',
+	                             sep = '\t')
+	pos = {n : (taz.node[n]['lat'], taz.node[n]['lon']) for n in taz}
+	nx.set_node_attributes(taz, 'pos', pos)
+
 	print 'Reading metro network.'
 	metro = utility.read_metro(directory = metro_dir, file_prefix = metro_prefix) # networkx graph
 
 	print 'Reading street network'
 	streets = utility.read_streets(directory = street_dir, file_prefix = street_prefix) # networkx graph
 
-	layer_dict = {'metro' : metro, 'streets' : streets} # need a dict to add to multiplex.
+
+
+	layer_dict = {'metro' : metro, 'streets' : streets, 'taz' : taz} # need a dict to add to multiplex.
 
 	multi = mx.multiplex() # initialize empty multiplex
 	multi.add_layers(layer_dict) # add metro and street layers to multiplex
+	print multi.get_layers()
 	multi.spatial_join(layer1 = 'metro', layer2 = 'streets', transfer_speed = .1, base_cost = 0, both = True)
+	multi.spatial_join(layer1 = 'taz', layer2 = 'streets', transfer_speed = 10000000000000000, base_cost = 0, both = True)
 
 	multi.to_txt('2. multiplex', 'multiplex')
 
