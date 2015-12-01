62d094b011c7392fe56abc0ef6e1a1b515d921a9
Phil Chodrow <pchodrow@mit.edu>
2015-12-01 16:27:54 -0500

    igraph method runs assignment both with and without metro

--- assign_traffic.py
+++ assign_traffic.py
@@ -8,8 +8,10 @@ import networkx as nx
 def main():
 	if len(sys.argv) <= 1:
 		print 'Please choose one of \n homebrew \n igraph \n both'
-	
+		return None
+
 	P = [.4, .3, .2, .1]
+
 	attrname = 'congested_time_m'
 	second = ""
 
@@ -29,15 +31,26 @@ def main():
 			 P = P,
 			 exclude_layers = ['metro'],
 			 base_cost = 'free_flow_time_m')
-		
+	
+	# igraph method
 	if sys.argv[1] == 'igraph' or sys.argv[1] == 'both':
-		print 'computing ITA using igraph method'
+		print 'computing ITA using igraph method, no metro layer'
 		ITA2(multi, 
 			 layers = ['streets', 'taz'], 
 			 od_loc = '1_data/taz_od/0_1.txt',
 			 P = P,
 			 scale = .25,
-			 attrname = 'congested_time_m' + second)
+			 attrname = 'congested_time_m' + second,
+			 flow_name = 'flow')
+
+		print 'computing ITA using igraph method with metro layer'
+		ITA2(multi, 
+			 layers = ['streets', 'taz', 'metro'], 
+			 od_loc = '1_data/taz_od/0_1.txt',
+			 P = P,
+			 scale = .25,
+			 attrname = 'congested_time_metro_m' + second,
+			 flow_name = 'flow_metro')
 
 	if sys.argv[1] == 'both':
 		ratios = {e : (multi.G.edge[e[0]][e[1]]['congested_time_m_1'] 
@@ -50,23 +63,35 @@ def main():
 
 	io.multiplex_to_txt(multi, '2_multiplex/', 'multiplex_unscaled')
 
-def ITA1(multi, od_loc = '1_data/taz_od/0_1.txt', attrname = 'congested_time_m', **kwargs):
+def ITA1(multi, od_loc = '1_data/taz_od/0_1.txt', attrname = 'congested_time_m',  **kwargs):
 	G = multi.as_graph()
 	od = assignment.od_dict(G, '1_data/taz_od/0_1.txt')
 	assignment.geo_betweenness_ITA(multi, 
 								   OD = od,
 								   attrname = attrname,
 								   **kwargs)
+	
+def ITA2(multi, layers = ['streets', 'taz'], od_loc = '1_data/taz_od/0_1.txt', attrname = 'congested_time_m', flow_name = 'flow', **kwargs):
+	
+	# get the layers we'll be working with as an nx graph
+	sublayers = multi.layers_as_subgraph(layers)
 
-def ITA2(multi, layers = ['streets'], od_loc = '1_data/taz_od/0_1.txt', attrname = 'congested_time_m', **kwargs):
-	nx.set_edge_attributes(multi.G, attrname, nx.get_edge_attributes(multi.G, 'free_flow_time_m'))
-	g = utility.nx_2_igraph(multi.layers_as_subgraph(layers))
+	nx.set_edge_attributes(multi.G, attrname, nx.get_edge_attributes(multi.G, 'free_flow_time_m')) 
+	
+	# ----------------
+	# Alternative to above line: sets the 'base' time only for the specified sublayers. Layers of the multiplex not specified will have this attribute equal to None.
+	# nx.set_edge_attributes(multi.G, attrname, nx.get_edge_attributes(sublayers, 'free_flow_time_m'))
+	
+	nx.set_edge_attributes(multi.G, flow_name, 0)
+
+	# Execute the assignment algorithm
+	g = utility.nx_2_igraph(sublayers)
 	od = assignment.od_dict_igraph(g, od_loc)
-	assignment.ITA_igraph(g = g, od = od, attrname = 'congested_time_m', **kwargs)
-	d = {(g.vs[g.es[i].source]['id'], g.vs[g.es[i].target]['id']) : g.es[i]['congested_time_m'] for i in range(len(g.es))}
+	assignment.ITA_igraph(g = g, od = od, attrname = attrname, **kwargs)
+	d = {(g.vs[g.es[i].source]['id'], g.vs[g.es[i].target]['id']) : g.es[i][attrname] for i in range(len(g.es))}
+	f = {(g.vs[g.es[i].source]['id'], g.vs[g.es[i].target]['id']) : g.es[i]['flow'] for i in range(len(g.es))}
 	nx.set_edge_attributes(multi.G, attrname, d)
-
-
+	nx.set_edge_attributes(multi.G, flow_name, f)
 
 if __name__ == '__main__':
 	main()
