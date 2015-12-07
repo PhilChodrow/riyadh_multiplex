ae66e9234ddc4a8035783bfb6387b714c667390c
Phil Chodrow <pchodrow@mit.edu>
2015-12-07 12:05:04 -0500

    added reducing_congestion.ipynb

--- metro/analysis.py
+++ metro/analysis.py
@@ -3,6 +3,7 @@ from metro import utility
 import networkx as nx
 import numpy as np
 
+
 def distance(pos1,pos2):
 	"""Compute geographical distance between two points
 	
@@ -33,7 +34,7 @@ def gini_coeff(x):
 	r = np.argsort(np.argsort(-x)) # calculates zero-based ranks
 	return 1 - (2.0 * (r*x).sum() + s)/(n*s)
 
-def igraph_betweenness_centrality(self, layers = None, weight = None, attrname = 'bc'):
+def igraph_betweenness_centrality(multi, layers = None, weight = None, attrname = 'bc'):
 		'''
 		compute the (weighted) betweenness centrality of one or more layers and save to self.G.node attributes. 
 		args: 
@@ -41,27 +42,28 @@ def igraph_betweenness_centrality(self, layers = None, weight = None, attrname =
 			source_layers -- the layers to use as sources in betweenness calculation.
 			target_layers -- the layers to use as targets in the betweenness calculation.  
 		'''
-		print 'Computing betweenness centrality -- this could take a while.' 
 
-		g = utility.nx_2_igraph(self.layers_as_subgraph(layers))
+		g = utility.nx_2_igraph(multi.layers_as_subgraph(layers))
 
 		bc = g.betweenness(directed = True,
 						  cutoff = 300,
 						  weights = weight)
-		print 'betweenness calculated'
+
 		d = dict(zip(g.vs['name'], bc))
 		d = {key:d[key] for key in d.keys()}
+
+		nx.set_node_attributes(multi.G, attrname, d)
 
 def local_intermodality(self, layer = None, thru_layer = None, weight = None):
 	"""Compute the local intermodality of a set of nodes and save as a node attribute. 
 	
 	Args:
-	    layer (str, optional): the layer for which to compute intermodality
-	    thru_layer (str, optional): the layer through which a path couns as 'intermodal'
-	    weight (str, optional): the numeric edge attribute used to weight paths
+		layer (str, optional): the layer for which to compute intermodality
+		thru_layer (str, optional): the layer through which a path couns as 'intermodal'
+		weight (str, optional): the numeric edge attribute used to weight paths
 	
 	Returns:
-	    None
+		None
 	"""
 	g = utility.nx_2_igraph(self.G)
 	nodes = g.vs.select(layer=layer)
@@ -85,10 +87,10 @@ def spatial_outreach(multi, node_layer = 'taz', thru_layers = ['streets'], weigh
 	Currently uses area of convex hull to measure outreach.
 	
 	Args:
-	    layer (TYPE, optional): the layer in which to compute spatial outreach
-	    weight (TYPE, optional): the numeric edge attribute by which to measure path lengths
-	    cost (TYPE, optional): the maximum path length 
-	    attrname (str, optional): the base name to use when saving the computed outreach
+		layer (TYPE, optional): the layer in which to compute spatial outreach
+		weight (TYPE, optional): the numeric edge attribute by which to measure path lengths
+		cost (TYPE, optional): the maximum path length 
+		attrname (str, optional): the base name to use when saving the computed outreach
 	'''
 	from shapely.geometry import MultiPoint
 	
@@ -121,11 +123,11 @@ def proximity_to(multi, layers, to_layer):
 	is measured as Euclidean distance, not graph distance. 
 	
 	Args:
-	    layers (TYPE): base layer from which to compute proximity
-	    to_layer (TYPE): layer to which to calculate proximity 
+		layers (TYPE): base layer from which to compute proximity
+		to_layer (TYPE): layer to which to calculate proximity 
 	
 	Returns:
-	    TYPE: Description
+		TYPE: Description
 	"""
 	layers_copy = multi.layers_as_subgraph(layers)	
 	to_layer_copy = multi.layers_as_subgraph([to_layer])
@@ -133,79 +135,82 @@ def proximity_to(multi, layers, to_layer):
 	nx.set_node_attributes(multi.G, 'proximity_to_' + to_layer, d)
 
 def accessible_nodes(self, origin, weight, limit):
-        '''
-        Returns a dictionary of all nodes that can be accessed within the given limit according to the specified weight
-        
-        Args:
-    origin (str): the source node
-    weight (str): the edge weight by which to compute shortest paths.
-    limit (float): the upper bound for accessible shortest paths
-                
+		'''
+		Returns a dictionary of all nodes that can be accessed within the given limit according to the specified weight
+		
+		Args:
+	origin (str): the source node
+	weight (str): the edge weight by which to compute shortest paths.
+	limit (float): the upper bound for accessible shortest paths
+				
 Returns:
-    dict: a dictionary of shortest path lengths (in the given weight) indexed by the destination node
-        '''
-        q = [ (0, origin, None) ]
-        seen = {}
-        while q:
-            dist, current, parent = heappop(q)
-            if dist > limit: break
-            seen[current] = dist
-            for nextNode, edge in self.G[current].items():
-                if nextNode in seen: continue
-                heappush(q, (dist + edge[weight], nextNode, current) )  
-        return seen
+	dict: a dictionary of shortest path lengths (in the given weight) indexed by the destination node
+		'''
+		q = [ (0, origin, None) ]
+		seen = {}
+		while q:
+			dist, current, parent = heappop(q)
+			if dist > limit: break
+			seen[current] = dist
+			for nextNode, edge in self.G[current].items():
+				if nextNode in seen: continue
+				heappush(q, (dist + edge[weight], nextNode, current) )  
+		return seen
 
 def weighted_betweenness(g, od, weight = 'free_flow_time_m',scale = .25, attrname = 'weighted_betweenness'):
-    vs = g.vs
-    es = g.es
-    
-    # initialize graph attributes for collecting later
-    vs[attrname] = 0
-    
-    # collects flows
-    node_dict = collections.defaultdict(int)
 
-    # main assignment loop
-    start = time.clock()
-    for o in od:
-        ds = od[o]
-        if len(ds) > 0:
-            targets = ds.keys()
-            paths = g.get_shortest_paths(o, 
-                                         to=targets, 
-                                         weights=weight, 
-                                         mode='OUT', 
-                                         output="vpath") # compute paths
-            for path in paths:
-                if len(path) > 0:
-                    flow = ds[path[-1:][0]]
-                    for v in path: 
-                        node_dict[v] += scale * flow
-                    
-    print 'betweenness calculated in in ' + str(round((time.clock() - start) / 60.0,1)) + 'm'
-    for key in node_dict:
-        vs[key][attrname] = node_dict[key]
+	import collections
+	
+	vs = g.vs
+	es = g.es
+	
+	# initialize graph attributes for collecting later
+	vs[attrname] = 0
+	
+	# collects flows
+	node_dict = collections.defaultdict(int)
+
+	# main assignment loop
+	
+	for o in od:
+		ds = od[o]
+		if len(ds) > 0:
+			targets = ds.keys()
+			paths = g.get_shortest_paths(o, 
+										 to=targets, 
+										 weights=weight, 
+										 mode='OUT', 
+										 output="vpath") # compute paths
+			for path in paths:
+				if len(path) > 0:
+					flow = ds[path[-1:][0]]
+					for v in path: 
+						node_dict[v] += scale * flow
+					
+
+	for key in node_dict:
+		vs[key][attrname] = node_dict[key]
 
 def path_lengths_igraph(g, nodes, weight, mode = 'array'):
-    '''
-    quick finding of shortest path lengths between nodes. 
-    If as_df, returns as a pd dataframe with columns for origin and destination. 
-    If not as_df, returns a 1d np.array(). This is significantly faster in situations when we don't need
-    to keep track of o and d. 
-    '''
-    lengths = g.shortest_paths_dijkstra(weights = weight, source = nodes, target = nodes)
-    if mode == 'df':
-        q = [(nodes[i],nodes[j],lengths[i][j]) for i in range(len(nodes)) for j in range(len(nodes))]
-        o = [tup[0] for tup in q]
-        d = [tup[1] for tup in q]
-        p = [tup[2] for tup in q]
-        df = pd.DataFrame({'o' : o, 'd' : d, weight + '_length' : p})
-        return lengths
-    elif mode == 'array':
-        lengths = np.array(lengths)
-        return lengths.ravel()
-    else:
-    	return lengths
+	'''
+	quick finding of shortest path lengths between nodes. 
+	If as_df, returns as a pd dataframe with columns for origin and destination. 
+	If not as_df, returns a 1d np.array(). This is significantly faster in situations when we don't need
+	to keep track of o and d. 
+	'''
+	lengths = g.shortest_paths_dijkstra(weights = weight, source = nodes, target = nodes)
+	if mode == 'df':
+		q = [(nodes[i],nodes[j],lengths[i][j]) for i in range(len(nodes)) for j in range(len(nodes))]
+		o = [tup[0] for tup in q]
+		d = [tup[1] for tup in q]
+		p = [tup[2] for tup in q]
+		df = pd.DataFrame({'o' : o, 'd' : d, weight + '_length' : p})
+		return lengths
+	elif mode == 'array':
+		lengths = np.array(lengths)
+		return lengths.ravel()
+	else:
+		return lengths
 
 def standardize(array):
-    return (array - array.mean()) / array.std()
+	return (array - array.mean()) / array.std()
