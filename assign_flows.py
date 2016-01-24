b8b5a9f657034863bd1a6ddca571bdbb1958b714
Phil Chodrow <pchodrow@mit.edu>
2016-01-24 12:48:29 -0500

    some comments on assign_flows.py

--- assign_flows.py
+++ assign_flows.py
@@ -1,5 +1,4 @@
 from metro import multiplex as mx
-from metro import assignment
 from metro import utility
 from metro import ita
 
@@ -9,55 +8,73 @@ import cProfile
 import time
 import networkx as nx
 
-multi = mx.read_multi()
 
-# Scale metro so that it runs at the mean street speed. 
-dists = nx.get_edge_attributes(multi.G, 'dist_km')
-times = nx.get_edge_attributes(multi.G, 'free_flow_time_m')
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+def main():
 
-speed_km_m = {key : dists[key]/times[key] for key in dists}
+    # Read in the multiplex
+    m = mx.read_multi()
 
-nx.set_edge_attributes(multi.G, 'speed_km_m', speed_km_m)
+    # Scale metro so that it runs at the mean street speed. 
+    dists = nx.get_edge_attributes(m.G, 'dist_km')
+    times = nx.get_edge_attributes(m.G, 'free_flow_time_m')
 
-street_speed = multi.mean_edge_attr_per(layers = ['streets'], attr = 'speed_km_m', weight_attr = 'dist_km')
-metro_speed = multi.mean_edge_attr_per(layers = ['metro'], attr = 'speed_km_m', weight_attr = 'dist_km') 
+    speed_km_m = {key : dists[key]/times[key] for key in dists}
 
-scale = street_speed / metro_speed
+    nx.set_edge_attributes(m.G, 'speed_km_m', speed_km_m)
 
-multi.scale_edge_attribute(layer = 'metro', attribute = 'free_flow_time_m', beta = 1/scale)
+    street_speed = m.mean_edge_attr_per(layers = ['streets'], 
+                                        attr = 'speed_km_m', 
+                                        weight_attr = 'dist_km')
 
+    metro_speed = m.mean_edge_attr_per(layers = ['metro'], 
+                                       attr = 'speed_km_m', 
+                                       weight_attr = 'dist_km') 
 
-# Read OD
-multi.read_od(layer = 'taz', key = 'taz', od_file = '1_data/taz_od/0_1.txt', sep = " ")
+    scale = street_speed / metro_speed
 
-betas = [100, 10, 1, .9, .8, .7, .6, .5, .4, .3, .2, .1, .01]
+    m.scale_edge_attribute(layer = 'metro', 
+                           attribute = 'free_flow_time_m', 
+                           beta = 1/scale)
 
-for beta in betas:
+    # Read OD keyed to m
+    m.read_od(layer = 'taz', 
+              key = 'taz', 
+              od_file = '1_data/taz_od/0_1.txt', 
+              sep = " ")
 
-  if beta in [100, 1, .5, .1, .01]:
-    summary = True
-  else:
-    summary = False
+    # main loop: for each beta, scale the metro layer of m, run ITA, and save
+    # the results. Note that using summary = True prints out route-wise 
+    # summaries for each level of beta, and is VERY computationally expensive. 
+    betas = [100, 10, 1, .9, .8, .7, .6, .5, .4, .3, .2, .1, .01]
 
-  start = time.clock()
-  multi.scale_edge_attribute(layer = 'metro',
+    for beta in betas:
+
+      start = time.clock()
+      m.scale_edge_attribute(layer = 'metro',
+                                 attribute = 'free_flow_time_m',
+                                 beta = beta)
+
+      df = m.run_ita(n_nodes = None, 
+                    summary = True, # change this to get route tables 
+                    attrname = 'congested_time_m_' + str(beta),
+                    flow_name = 'flow_' +str(beta),
+                    P = [.2, .2, .2, .2, .1, .1],
+                    scale = .25)
+
+      if df is not None:
+        df.to_csv('3_throughput/route_info_' + str(beta) + '.csv')
+
+      m.scale_edge_attribute(layer = 'metro',
                              attribute = 'free_flow_time_m',
-                             beta = beta)
+                             beta = 1.0/beta)
 
-  df = multi.run_ita(n_nodes = None, 
-                summary = summary, # change this to get route tables 
-                attrname = 'congested_time_m_' + str(beta),
-                flow_name = 'flow_' +str(beta),
-                P = [.2, .2, .2, .2, .1, .1],
-                scale = .25)
+      time_taken = str(round((time.clock() - start) / 60.0, 1)) + 'm'
+      print 'assignment for beta = ' + str(beta) + ' completed in ' + time_taken
 
-  if df is not None:
-    df.to_csv('3_throughput/route_info_' + str(beta) + '.csv')
+    m.to_txt('3_throughput/', 'mx_flow')
 
-  multi.scale_edge_attribute(layer = 'metro',
-                         attribute = 'free_flow_time_m',
-                         beta = 1.0/beta)
-
-  print 'assignment for beta = ' + str(beta) + ' completed in ' + str(round((time.clock() - start) / 60.0, 1)) + 'm'
-
-multi.to_txt('3_throughput/', 'mx_flow')
+if __name__ = '__main__':
+    main()
